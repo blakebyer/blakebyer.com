cbbcde596264529593195d77d8f33122043bc4e2
blakebyer <bbyer04@gmail.com>
2025-06-09 13:50:58 -0700

    new blog post and slider

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "/blog/2024/07/04/letourdecarrboro.html",
         "teaser": null
+      },{
+        "title": "GLBIO 2025",
+        "excerpt":"I spent last week in Minneapolis, Minnesota at the International Society for Computational Biology Great Lakes Bioinformatics Conference, aptly named GLBIO 2025. I presented a poster on Alzheimer’s disease drug repurposing using multi-ontology enrichment analysis, with an a priori gene set derived from AD GWAS. Using this method I prioritized...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/05/15/glbio25.html",
+        "teaser": null
       }]
