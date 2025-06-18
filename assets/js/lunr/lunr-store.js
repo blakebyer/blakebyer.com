c57e2166c0661f48cfa15f71237ed866a4c3b50d
blakebyer <blakebyer@users.noreply.github.com>
2025-06-18 04:53:51 +0000

    deploy: 994c281d3012d6bfcc0a02fe9d4b3c221e95e333

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Cuties",
+        "excerpt":"Only occasionally do I eat citrus fruits. Something about the hassle of peeling deters me. Orange zest underneath my fingernails, a thin mist of juice covers my desk. After I finish peeling, all that covers the orange is a thin layer of white pith. I do little to remove the...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2023/03/08/citrus.html",
+        "teaser": null
+      },{
+        "title": "Four on the Fourth",
+        "excerpt":"This morning, I ran the Four on the Fourth Race in Carrboro, North Carolina. This race was a part of le Tour de Carrboro series. I placed second overall in the four-mile race, with a time of 20:44. I had a faster time and the win in mind, but I...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2024/07/04/letourdecarrboro.html",
+        "teaser": null
+      },{
+        "title": "GLBIO 2025",
+        "excerpt":"I spent last week in Minneapolis, Minnesota at the International Society for Computational Biology Great Lakes Bioinformatics Conference, aptly named GLBIO 2025. I presented a poster on Alzheimer’s disease drug repurposing using multi-ontology enrichment analysis, with an a priori gene set derived from AD GWAS. Using this method I prioritized...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/05/15/glbio25.html",
+        "teaser": null
+      },{
+        "title": "Neuroscience for Beginners",
+        "excerpt":"What is your brain? The brain is a 3-pound organ about the size of two balled up fists sitting within your skull (cranium). It is the gatekeeper to your conscious experience of reality, the source of your intellect, and the lifeforce of your bodily functions. Two schematic representations of the...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/06/17/neurobeginners.html",
+        "teaser": null
+      },{
+        "title": "Cells of the Nervous System",
+        "excerpt":"Levels of Organization of Multicellular Organisms In order from smallest to largest, the body is composed of: Organelles: cellular components built for specific functions (e.g. endoplasmic reticulum). Cells: basic subunit of biology (e.g. nerve cell). Tissue: a group of similar cells that perform a specific function (e.g. smooth muscle). Organ:...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/06/17/neurocells.html",
+        "teaser": null
+      }]
