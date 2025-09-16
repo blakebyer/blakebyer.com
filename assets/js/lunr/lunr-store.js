503ae5ccf39092d3a73b0816e9ee50b81be423bc
blakebyer <blakebyer@users.noreply.github.com>
2025-09-16 01:56:45 +0000

    deploy: 28db99ab71d477681fec5c3feba332f997d381c8

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,49 +1,7 @@
 var store = [{
-        "title": "Cuties",
-        "excerpt":"Only occasionally do I eat citrus fruits. Something about the hassle of peeling deters me. Orange zest underneath my fingernails, a thin mist of juice covers my desk. After I finish peeling, all that covers the orange is a thin layer of white pith. I do little to remove the...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/2023/03/08/citrus.html",
-        "teaser": null
-      },{
-        "title": "Four on the Fourth",
-        "excerpt":"This morning, I ran the Four on the Fourth Race in Carrboro, North Carolina. This race was a part of le Tour de Carrboro series. I placed second overall in the four-mile race, with a time of 20:44. I had a faster time and the win in mind, but I...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/2024/07/04/letourdecarrboro.html",
-        "teaser": null
-      },{
         "title": "GLBIO 2025",
         "excerpt":"I spent last week in Minneapolis, Minnesota at the International Society for Computational Biology Great Lakes Bioinformatics Conference, aptly named GLBIO 2025. I presented a poster on Alzheimer’s disease drug repurposing using multi-ontology enrichment analysis, with an a priori gene set derived from AD GWAS. Using this method I prioritized...","categories": ["blog"],
         "tags": [],
         "url": "/blog/2025/05/15/glbio25.html",
-        "teaser": null
-      },{
-        "title": "Neuroscience for Beginners",
-        "excerpt":"What is your brain? The brain is a 3-pound organ about the size of two balled up fists sitting within your skull (cranium). It is the gatekeeper to your conscious experience of reality, the source of your intellect, and the lifeforce of your bodily functions. Two schematic representations of the...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/2025/06/17/neurobeginners.html",
-        "teaser": null
-      },{
-        "title": "Cells of the Nervous System",
-        "excerpt":"Levels of Organization in Multicellular Organisms In order from smallest to largest, an organism is composed of: Organelles: cellular components built for specific functions (e.g. endoplasmic reticulum). Cells: basic subunit of biology (e.g. nerve cell). Tissue: a group of similar cells that perform a specific function (e.g. smooth muscle). Organ:...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/2025/06/18/neurocells.html",
-        "teaser": null
-      },{
-        "title": "Math Notation Sheet",
-        "excerpt":"Algebra Symbols Notation Meaning \\(\\sum_{i=1}^{n} x_i\\) Sum \\(\\prod_{i=1}^{n} x_i\\) Product Probability and Statistics Notation Meaning \\(P(A)\\) Probability of A \\(P(A \\cap B)\\) A and B (intersection) \\(P(A \\cup B)\\) A or B (union) \\(P(A \\mid B)\\) Conditional probability \\(\\mu\\) Mean (population) \\(\\bar{x}\\) Mean (sample) \\(\\sigma\\) Standard deviation \\(\\sigma^2\\) Variance Linear...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/2025/07/30/mathnotation.html",
-        "teaser": null
-      },{
-        "title": "Introduction to Statistics",
-        "excerpt":"Introduction Statistics is the backbone of impactful, reproducible research. Properly done, statistics provide us with basis for cause and effect (known as causal inference). I wanted to write a few supplementary articles for myself and others to understand the basics of probability, statistics, and linear algebra to boost their research....","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/2025/07/30/statsintro.html",
-        "teaser": null
-      },{
-        "title": "Introduction to Probability",
-        "excerpt":"The Birthday Paradox Consider the question, how many people must be a room such that the probability of at least two will share a birthday exceeds 50%? You might guess about 182 because it’s about half the days in a year, or 100 because it’s a nice round number. However,...","categories": ["blog"],
-        "tags": [],
-        "url": "/blog/2025/07/31/probabilityintro.html",
         "teaser": null
       }]
